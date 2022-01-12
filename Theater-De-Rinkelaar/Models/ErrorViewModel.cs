d5a6413c699bca94c298caf5cc25d7d8e74355bc
Brendan Darwinkel <110816@spinoza.local>
2022-01-12 12:32:14 +0100

    Add project files.

---
+++ Theater-De-Rinkelaar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Theater_De_Rinkelaar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
