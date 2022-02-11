ebc92d82844bb4a2137350f6f2d39f6ecc9747bb
V. Speijer <110664@st-maartenscollege.nl>
2022-02-11 10:05:43 +0100

    Database gekoppeld, topnav title allignment

--- Theater-De-Rinkelaar/Controllers/HomeController.cs
+++ Theater-De-Rinkelaar/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Theater_De_Rinkelaar.Models;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Theater_De_Rinkelaar.Controllers
-{
+{    
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -20,7 +22,11 @@ namespace Theater_De_Rinkelaar.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // alle namen ophalen
+            var names = GetNames();
+
+            // stop de namen in de html
+            return View(names);
         }
 
         [Route("Agenda")]
@@ -39,6 +45,42 @@ namespace Theater_De_Rinkelaar.Controllers
         public IActionResult Enquête()
         {
             return View();
+        }
+
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110664;Uid=110664;Pwd=inf2122sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM `voorstellingen`", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["beschrijving_lang"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
