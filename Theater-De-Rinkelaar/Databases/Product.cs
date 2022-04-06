2c11a1fcad1b6e9b531d4efef0a2e8115dc66ffe
Vincent Rink <110698@st-maartenscollege.nl>
2022-04-06 10:04:27 +0200

    Geprobeerd de database te fixen

---
+++ Theater-De-Rinkelaar/Databases/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Theater_De_Rinkelaar.Database;
+
+namespace Theater_De_Rinkelaar.Databases
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Naam { get; set; }
+
+        public string? Prijs { get; set; }
+
+        public int Beschikbaarheid { get; set; }
+    }
+
+    
+}
+
